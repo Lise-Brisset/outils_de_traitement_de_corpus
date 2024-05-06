1c648ce58b855e585dfdadd1408866b7ab40e3e1
Lise-Brisset <lise.brisset9@gmail.com>
2024-05-06 13:47:22 +0200

    fix: ajout des docstrings

--- scripts/process/scrapper.py
+++ scripts/process/scrapper.py
@@ -1,3 +1,18 @@
+"""
+
+Ce programme utilise les librairies requests et lxml pour faire du scrapping web. Il part de l'url Wikipédia sur les
+chats. Il récupère les contenus des introductions de cette page et de toutes les urls hypertexts wikipédia
+qu'il peut y rencontrer. Il s'arrête au bout de 50 liens hypertexts.
+
+Ce programme peut aussi être importer comme un module car il contient les focntions suivantes :
+    * get_urls() - retourne tous les liens href des balises <a> de l'url
+    * get_first_header_text() - retourne tous les contenus textuels d'intro de chaque url
+    * save_txt() - sauvegarde les contenus textuels dans des fichiers textes
+    * save_all_urls_contents() - lance et accorde toutes les focntions ci-dessus.
+
+"""
+
+
 import requests
 import lxml.html
 
@@ -22,7 +37,14 @@ introduction = root.xpath("//div[@id='bodyContent']//p|//div[@id='bodyContent']/
 # print(introduction)
 
 def get_urls():
-    """ vient récupèrer tous les liens présents sur la page de départ """
+    """ Vient récupèrer tous les liens présents sur la page de départ.
+    Parameters
+    ----------
+    Returns
+    -------
+    dict
+        Un dictionnaire de clé = id et valeur = url.
+    """
     dico_urls = {}
     id=1
     for p in introduction:
@@ -46,8 +68,18 @@ urls = get_urls()
 
 
 def get_first_header_text(root):
-    """ récupère le contenu textuel de l'introduction de la page visitée """
-    intro = root.xpath("//div[@id='bodyContent']//p|//div[@id='bodyContent']//h2") # on récupère les éléments qui nous intéressent
+    """ Récupère le contenu textuel de l'introduction de la page visitée.
+    Parameters
+    ----------
+    root : str
+        Le contenu de la toute première page de départ.
+    Returns
+    -------
+    str
+        Le contenu textuel de l'introduction de la page.
+    """
+    # on récupère les éléments qui nous intéressent
+    intro = root.xpath("//div[@id='bodyContent']//p|//div[@id='bodyContent']//h2")
 
     text = []
 
@@ -61,22 +93,43 @@ def get_first_header_text(root):
 #print(get_first_header_text(root))
 
 def save_txt(text_content, file_name):
-    """ enregistre sous format txt le contenu textuel de la page visitée """
+    """ Enregistre sous format txt le contenu textuel de la page visitée.
+    Parameters
+    ----------
+    text_content : str
+        Le contenu textuel à enregistrer.
+    file_name : str
+        Le nom souhaité pour le document que l'on enregistre.
+    Returns
+    -------
+    file
+        Enregistre le fichier dans le dossier data/clean/.
+    """
     with open(f"../../data/clean/{file_name}", "w") as fichier:
         fichier.write(text_content)
 
 
 def save_all_urls_contents(urls_dico):
-    """ Permet de parcourir toutes nos urls et d'enregistrer automatiquement les contenus de chacune de ces pages dans des fichiers textes."""
+    """ Lance et combine l'ensemble des fonctions précédantes.
+    Parameters
+    ----------
+    urls_dico : dict
+        Le dictionnaire clé = id et valeur = url.
+    Returns
+    -------
+    file
+        Enrgistre tous les contenus textuels d'intro de chaque urls dans des fichiers textes.
+    """
     compte = 0
     for url in urls_dico.values():
-        # condition d'arrêt de la boucle ajoutée pour ne pas récupérer plus de 50 pages, plus n'étant pas spécialement utile dans ce projet
+        # condition d'arrêt de la boucle ajoutée pour ne pas récupérer plus de 50 pages
         if compte <= 50:
             req = requests.get(url[0])
             the_root = lxml.html.fromstring(req.content)
             # on récupère le contenu de l'url sur laquelle nous sommes
             content = get_first_header_text(the_root)
-            # on peut rencontrer des contenus vides (page pas non remplie comme celle de https://fr.wikipedia.org/wiki/Mathou
+            # on peut rencontrer des contenus vides
+            # (page pas non remplie comme celle de https://fr.wikipedia.org/wiki/Mathou)
             if content is not None:
                 # on enregistre sous format txt le contenu de l'url
                 save_txt(content, f"{url[1]}.txt")
